ca2407d3ad49610a7e90817383767073915c6e84
SamyG12 <90658089+SamyG12@users.noreply.github.com>
2022-01-07 18:34:31 -0500

    first commit

---
+++ password_generator/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from password_generator_app import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('password/', views.password, name='password'),
+    path('about/', views.about, name='about'),
+    
+]
